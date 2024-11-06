834299541073427e6d352f360c582be68c812218
chirag2314 <chirag230102@gmail.com>
2024-11-07 02:04:39 +0530

    Upto Admin Dashboard

---
+++ models.py
@@ -0,0 +1,83 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+from werkzeug.security import generate_password_hash, check_password_hash
+db = SQLAlchemy(app)
+
+#Database Modelling
+
+class Customer(db.Model):
+    __tablename__='customer'
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(32), nullable=False, unique=True)
+    passwordhash=db.Column(db.String(512), nullable=False)
+    name=db.Column(db.String(64), nullable=False)
+    utype=db.Column(db.String(16), nullable=False, default='Customer')
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.passwordhash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.passwordhash, password)
+
+class Professional(db.Model):
+    __tablename__='professional'
+    id=db.Column(db.Integer, primary_key=True)
+    username=db.Column(db.String(32), nullable=False, unique=True)
+    passwordhash=db.Column(db.String(512), nullable=False)
+    name=db.Column(db.String(64), nullable=False)
+    contact=db.Column(db.Integer, nullable=False)
+    service_id=db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    experience=db.Column(db.Integer, nullable=False)
+    pincode=db.Column(db.String(8), nullable=False)
+    utype=db.Column(db.String(16), nullable=False, default='Professional')
+    status=db.Column(db.Boolean, nullable=True, default='Pending')
+    profile=db.Column(db.String(512), nullable=False)
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    @password.setter
+    def password(self,password):
+        self.passwordhash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.passwordhash, password)
+
+class Service(db.Model):
+    __tablename__='service'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(32),nullable=False)
+    price=db.Column(db.Integer, nullable=False)
+    description=db.Column(db.String(512), nullable=False)
+    time=db.Column(db.Integer,nullable=False)
+
+    def get_service_id(self, service):
+        return id
+
+class ServiceRequest(db.Model):
+    __tablename__='servicerequests'
+    servicereqid=db.Column(db.Integer,primary_key=True)
+    cuser=db.Column(db.String(32),db.ForeignKey('customer.username'),nullable=False)
+    puser=db.Column(db.String(32),db.ForeignKey('professional.username'),nullable=False)
+    service_id=db.Column(db.Integer, db.ForeignKey('service.id'), nullable=False)
+    requestdate=db.Column(db.DateTime, nullable=False)
+    completedate=db.Column(db.DateTime, nullable=False)
+    status=db.Column(db.String(32), nullable=False)
+    rating=db.Column(db.String(32), nullable=False)
+    review=db.Column(db.String(512), nullable=True)
+    datetime=db.Column(db.DateTime,nullable=False)
+
+with app.app_context():
+    db.create_all()
+    cadmin = Customer.query.filter_by(username='admin').first()
+    if not cadmin:
+        admin=Customer(username='admin', password='admin', name='admin', is_admin=True)
+        db.session.add(admin)
+        db.session.commit()
---
+++ routes.py
@@ -0,0 +1,198 @@
+from functools import wraps
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+import os
+
+from models import db, Customer, Professional, Service, ServiceRequest
+
+from app import app
+
+type='C'
+
+
+def auth_required(func):
+    @wraps(func)
+    def inner(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('Please Login')
+            return redirect(url_for('login'))
+        return func(*args, **kwargs)
+    return inner
+
+@app.route('/')
+@auth_required
+def defaultpage():
+    if type=='P':
+        return render_template('defaultpage.html', user=Professional.query.get(session['user_id']))
+    else:
+        user=Customer.query.get(session['user_id'])
+        if user.is_admin:
+            return redirect(url_for('admin'))
+        return render_template('defaultpage.html', user=Customer.query.get(session['user_id']))
+
+@app.route('/admin')
+@auth_required
+def admin():
+    user=Customer.query.get(session['user_id'])
+    if not user.is_admin:
+        flash('Unauthorized Access')
+        session.pop('user_id', None)
+        return redirect(url_for('login'))
+    return render_template('admin.html', user=user, service=Service.query.all())
+
+
+@app.route('/cprofile')
+@auth_required
+def cprofile():
+    user=Customer.query.get(session['user_id'])
+    if user.is_admin:
+        return redirect(url_for('admin'))
+    else:
+        return render_template('cprofile.html',  user=user)
+
+@app.route('/cprofile', methods=['POST'])
+@auth_required
+def cprofile_post():
+    user = Customer.query.get(session['user_id'])
+    username=request.form.get('username')
+    password=request.form.get('password')
+    if username == '' or password == '':
+        flash('Username of Password cannot be empty.')
+        return redirect(url_for('cprofile'))
+    if Customer.query.filter_by(username=username).first() and username != user.username:
+        flash('User exists, Please choose another username')
+        return redirect(url_for('cprofile'))       
+    user.username = username
+    user.password = password
+    db.session.commit()
+    flash('Details Updated successfully')
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    usertype=request.form.get('usertype')
+    username=request.form.get('username')
+    password=request.form.get('password')
+    if usertype=='professional':
+        if username == '' or password == '':
+            flash('Username or Password cannot be empty.')
+            return redirect(url_for('login'))
+        puser = Professional.query.filter_by(username=username).first()
+        if not puser:
+            flash('Incorrect Username or Password.')
+            return redirect(url_for('login'))
+        if not puser.check_password(password):
+            flash('Incorrect Username or Password.')
+            return redirect(url_for('login'))
+        session['user_id'] = puser.id
+        type='P'
+        return redirect(url_for('pprofile'))
+    if usertype=='customer':
+        if username == '' or password == '':
+            flash('Username or Password cannot be empty.')
+            return redirect(url_for('login'))
+        cuser = Customer.query.filter_by(username=username).first()
+        if not cuser:
+            flash('Incorrect Username or Password.')
+            return redirect(url_for('login'))
+        if not cuser.check_password(password):
+            flash('Incorrect Username or Password.')
+            return redirect(url_for('login'))
+        session['user_id'] = cuser.id
+        type='C'
+        return redirect(url_for('cprofile'))
+
+@app.route('/cregister')
+def cregister():
+    return render_template('cregister.html')
+
+@app.route('/pregister')
+def pregister():
+    return render_template('pregister.html')
+
+@app.route('/cregister', methods=['POST'])
+def cregister_post():
+    username=request.form.get('username')
+    password=request.form.get('password')
+    name=request.form.get('name')
+    if username == '' or password == '' or name == '':
+        flash('fields cannot be empty.')
+        return redirect(url_for('cregister'))
+    if Customer.query.filter_by(username=username).first():
+        flash('Username not available, please choose some other username')
+        return redirect(url_for('cregister'))
+    cuser=Customer(username=username, password=password, name=name)
+    db.session.add(cuser)
+    db.session.commit()
+    flash('Customer successfully Registered.')
+    return redirect(url_for('login'))
+
+@app.route('/pregister', methods=['POST'])
+def pregister_post():
+    username=request.form.get('username')
+    password=request.form.get('password')
+    name=request.form.get('name')
+    contact=request.form.get('contact')
+    service=request.form.get('service')
+    experience=request.form.get('experience')
+    pincode=request.form.get('pincode')
+    profile=request.files['profile']
+    if username == '' or password == '' or name == '' or contact == '' or service == '' or experience == '' or pincode == '':
+        flash('fields cannot be empty.')
+        return redirect(url_for('pregister'))
+    if Professional.query.filter_by(username=username).first():
+        flash('Username not available, please choose some other username')
+        return redirect(url_for('pregister'))
+    if not (profile and (profile.filename.endswith('.doc') or profile.filename.endswith('.docx') or profile.filename.endswith('.pdf'))):
+        flash('Invalid file, please upload .pdf, .doc or .docx files only')
+        return redirect(url_for('pregister'))
+    file_path = os.path.join('uploads', profile.filename)
+    profile.save(file_path) #doubtful
+    service_id=Service.query.get_service_id(service) #doubtful
+    status='Pending'
+    puser=Professional(username=username, password=password, name=name, contact=contact, service_id=service_id, experience=experience, pincode=pincode,status=status, profile=file_path)
+    db.session.add(puser)
+    db.session.commit()
+    flash('Professional successfully Registered')
+    return redirect(url_for('login'))
+
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
+@app.route('/services/addservice')
+@auth_required
+def add_services():
+    return render_template('services/addservice.html', user=Customer.query.get(session['user_id']))
+
+@app.route('/services/addservice', methods=['POST'])
+@auth_required
+def add_category_post():
+    name=request.form.get('servicename')
+    price=request.form.get('serviceprice')
+    description=request.form.get('servicedescription')
+    time=request.form.get('servicetime')
+    if name == '' or price == '' or description == '' or time == '':
+        flash('fields cannot be empty.')
+        return redirect(url_for('add_services'))
+    serv=Service(name=name,price=price,description=description,time=time)
+    db.session.add(serv)
+    db.session.commit()
+    flash('Service Added Successfully')
+    return redirect(url_for('admin'))
+
+
+@app.route('/services/<int:service_id>/edit')
+@auth_required
+def edit_service(service_id):
+    return ""
+
+@app.route('/services/<int:service_id>/delete')
+@auth_required
+def delete_service(service_id):
+    return ""
